5238c0649552fbea1d38577ec806eab102d79b16
Gastón Mitoire <gastonmm@gmail.com>
2023-07-18 12:46:13 -0300

    feat: banner sticky optional

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -6,11 +6,21 @@ interface BannerProps {
   url: string;
   title: string;
   className?: string;
+  sticky?: boolean;
 }
 
-export const Banner: React.FC<BannerProps> = ({ url, title, className }) => {
+export const Banner: React.FC<BannerProps> = ({
+  url,
+  title,
+  className,
+  sticky,
+}) => {
   return (
-    <div className={`${className} flex justify-center min-h-[100px] w-full`}>
+    <div
+      className={`${className} flex justify-center min-h-[100px] w-full border py-1.5 ${
+        sticky ? "sticky top-0" : ""
+      }`}
+    >
       <img src={url} alt={title} />
     </div>
   );
