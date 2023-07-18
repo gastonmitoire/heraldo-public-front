da1d46a73372ed9a2506307e606c1bc5f69b4c40
Gastón Mitoire <gastonmm@gmail.com>
2023-07-18 12:45:38 -0300

    feat: marquee sticky optional

--- app/components/Marquee.tsx
+++ app/components/Marquee.tsx
@@ -2,27 +2,30 @@ import React from "react";
 
 interface MarqueeProps {
   titles: string[];
+  clasName?: string;
 }
 
-export const Marquee: React.FC<MarqueeProps> = ({ titles }) => {
+export const Marquee: React.FC<MarqueeProps> = ({ titles, clasName }) => {
   return (
-    <div className="flex items-center gap-1 bg-red-500 px-16">
+    <div className={`${clasName} flex items-center gap-1 bg-red-500`}>
       <h5 className="text-xl uppercase font-bold opacity-50">Anticipo</h5>
-      <div className="overflow-x-hidden">
-        <div className="flex gap-3 py-3 animate-marquee whitespace-nowrap">
-          {titles.map((item, index) => (
-            <>
-              <span
-                key={index}
-                className="inline-block text-2xl text-white font-bold"
-              >
-                {item}
-              </span>
-              <span className="inline-block text-2xl text-white font-bold opacity-70">
-                /
-              </span>
-            </>
-          ))}
+      <div className="container mx-auto w-full flex">
+        <div className="overflow-x-hidden">
+          <div className="flex gap-3 py-3 animate-marquee whitespace-nowrap">
+            {titles.map((item, index) => (
+              <>
+                <span
+                  key={index}
+                  className="inline-block text-2xl text-white font-bold"
+                >
+                  {item}
+                </span>
+                <span className="inline-block text-2xl text-white font-bold opacity-70">
+                  /
+                </span>
+              </>
+            ))}
+          </div>
         </div>
       </div>
     </div>
