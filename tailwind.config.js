62d6af3efdda8e573beac953736dc3cd1fe509ed
Gastón Mitoire <gastonmm@gmail.com>
2023-07-28 10:19:29 -0300

    feat: fadeIn/fadeOut animations init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       animation: {
-        marquee: 'marquee 25s linear infinite',
+        marquee: "marquee 25s linear infinite",
+        fadeIn: "fadeIn 0.5s ease-in",
+        fadeOut: "fadeOut 0.5s ease-out",
       },
       keyframes: {
         marquee: {
-          '0%': { transform: 'translateX(100%)' },
-          '100%': { transform: 'translateX(-100%)' },
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+        fadeOut: {
+          "0%": { opacity: 1 },
+          "100%": { opacity: 0 },
         },
       },
     },
   },
   plugins: [],
-}
+};
