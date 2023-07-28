04fce3db97aa4217ac73ec75cf6737ab82060dd0
Gastón Mitoire <gastonmm@gmail.com>
2023-07-28 10:49:42 -0300

    feat: Image next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: [
+      "cms-el-heraldo.s3.us-east-1.amazonaws.com",
+      "cms-el-heraldo-prod.s3.us-east-1.amazonaws.com",
+      "cms-fatun.s3.us-east-1.amazonaws.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
