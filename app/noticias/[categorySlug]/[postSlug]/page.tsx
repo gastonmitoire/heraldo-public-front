ac856a663d482f5bd523f62bb6b726c5e7646adc
danielkraus14 <danielk14@servimatsrl.com.ar>
2023-08-02 09:31:52 -0300

    image, social icons, text and aside banner

--- app/noticias/[categorySlug]/[postSlug]/page.tsx
+++ app/noticias/[categorySlug]/[postSlug]/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 //Custom Components
 import { Banner } from "@/app/components/Banner";
-import { CardHighlight } from "@/app/components/CardHighlight";
+import { SocialMediaShareLinks } from "@/app/components/SocialMediaShareLinks";
 
 export default async function Page({
   params,
@@ -16,6 +16,7 @@ export default async function Page({
   );
 
   console.log(post);
+  const postContent = await JSON.parse(post.content);
 
   return (
     <div className="container flex flex-col gap-5 pt-5 mx-auto">
@@ -23,11 +24,9 @@ export default async function Page({
         <section className="grid grid-cols-1 gap-3 lg:col-span-3 ">
           <div className="flex justify-between pb-3 border-b-2 ">
             <p className="font-bold">
-            {post.category.name} - {post.section.name}
+              {post.category.name} - {post.section.name}
             </p>
-            <p>
-                {post.publicationDate}
-            </p>
+            <p>{post.publicationDate}</p>
           </div>
 
           <div className="flex-1">
@@ -41,8 +40,101 @@ export default async function Page({
               src={post.images[0].url}
               alt={post.title}
               fill
-              className="object-cover"
+              className="object-cover object-top"
             />
+          </div>
+          <div className="flex flex-col grid-cols-12 gap-3 pt-5 lg:grid">
+            <div className="col-span-1">
+              <SocialMediaShareLinks
+                title={post.title}
+                url={`http://localhost:3000/noticias/${post.category.slug}/${post.slug}`}
+                isVertical={true}
+                hasShareText={false}
+              />
+            </div>
+            <div className="col-span-10 text-left">
+              <p className="text-lg italic">{post.excerpt}</p>
+              {post.category.slug === "deportes" ? (
+                <iframe
+                  src={post.liveSports}
+                  width="100%"
+                  height="500px"
+                ></iframe>
+              ) : (
+                <>
+                  {postContent.blocks.map((block: any) => {
+                    if (block.type === "image") {
+                      return (
+                        <div className="mt-4">
+                          <img
+                            src={block.data.file.url}
+                            alt={block.data.caption}
+                          />
+                        </div>
+                      );
+                    }
+                    if (block.type === "paragraph") {
+                      return (
+                        <div className="mt-4"
+                          dangerouslySetInnerHTML={{ __html: block.data.text }}
+                        />
+                      );
+                    }
+                    if (block.type === "header") {
+                      return <h4 className="mt-4">{block.data.text}</h4>;
+                    }
+                    if (block.type === "list") {
+                      return (
+                        <ul className="mt-4">
+                          {block.data.items.map((item : any) => (
+                            <li className="mt-1">{item}</li>
+                          ))}
+                        </ul>
+                      );
+                    }
+                    if (block.type === "quote") {
+                      return <blockquote className="mt-4">{block.data.text}</blockquote>;
+                    }
+                    if (block.type === "delimiter") {
+                      return <hr />;
+                    }
+                    if (block.type === "raw") {
+                      return (
+                        <div className="mt-4"
+                          dangerouslySetInnerHTML={{ __html: block.data.html }}
+                        />
+                      );
+                    }
+                    if (block.type === "code") {
+                      return <code>{block.data.code}</code>;
+                    }
+                    if (block.type === "table") {
+                      return (
+                        <table className="mt-4">
+                          <thead>
+                            <tr>
+                              {block.data.content[0].map((item : any) => (
+                                <th>{item}</th>
+                              ))}
+                            </tr>
+                          </thead>
+                          <tbody>
+                            {block.data.content.slice(1).map((item : any) => (
+                              <tr>
+                                {item.map((td : any) => (
+                                  <td>{td}</td>
+                                ))}
+                              </tr>
+                            ))}
+                          </tbody>
+                        </table>
+                      );
+                    }
+                  })}
+                </>
+              )}
+            </div>
+            <div className="col-span-1">c</div>
           </div>
         </section>
         <aside className="flex flex-col">
