8d688129bd3a622fcbcc5f3c79c262cd5903462c
danielkraus14 <danielk14@servimatsrl.com.ar>
2023-08-05 13:02:53 -0300

    mobile styles to single post - added hightlight swiper, banners and PostsHighlight component to singlePost

--- app/components/BannerAdSense.tsx
+++ app/components/BannerAdSense.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React from "react";
+import Script from "next/script";
 
 interface BannerAdSenseProps {
   htmlContent: string;
@@ -29,11 +30,30 @@ const BannerAdSense: React.FC<BannerAdSenseProps> = ({
   }, []);
   return (
     <div
-      className={`${className} flex justify-center min-h-[100px] w-full py-1.5 ${
+      className={`${className} hidden justify-center min-h-[100px] w-full py-1.5 ${
         sticky ? "sticky top-0" : ""
       } ${border ? "border" : ""}`}
     >
-      {isMounted && <div dangerouslySetInnerHTML={{ __html: htmlContent }} />}
+      {isMounted && 
+      <>
+      <Script
+            id="adsense-right"
+            async
+            onError={() => console.log("error adsense right")}
+            strategy="afterInteractive"
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4444443092309144"
+            crossOrigin="anonymous"
+          />
+          <ins
+            className="adsbygoogle"
+            style={{display:'inline-block',width:'188px',height:'600px'}}
+            data-ad-client="ca-pub-4444443092309144"
+            data-ad-slot="1140012422"
+            data-ad-format="auto"
+            data-full-width-responsive="true"
+          ></ins>
+          <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+          </>}
     </div>
   );
 };
--- app/components/CardGridWithSwiper.tsx
+++ app/components/CardGridWithSwiper.tsx
@@ -18,68 +18,73 @@ interface CardGridWithSwiperProps {
   data: any[];
   className?: string;
   cardClassName?: string;
+  heading?: string;
 }
 
 export const CardGridWithSwiper: React.FC<CardGridWithSwiperProps> = ({
   data,
   className,
   cardClassName,
+  heading,
 }) => {
   return (
-    <Swiper
-      modules={[Navigation, Pagination, Scrollbar, A11y]}
-      spaceBetween={12}
-      slidesPerView={4}
-      navigation
-      className={`flex flex-col gap-3 ${className}`}
-      breakpoints={{
-        // when window width is >= 640px
-        320: {
-          slidesPerView: 1,
-        },
-        // small screens
-        640: {
-          slidesPerView: 1,
-        },
-        // medium screens
-        768: {
-          slidesPerView: 2,
-        },
-        // large screens
-        1024: {
-          slidesPerView: 3,
-        },
-        1280: {
-          slidesPerView: 4,
-        },
-        // extra large screens
-        1536: {
-          slidesPerView: 4,
-        },
-      }}
-    >
-      {!!data && data.length > 0 ? (
-        data.map((item: any) => (
-          <SwiperSlide
-            key={item._id}
-          >
-            <Card
-              item={{
-                title: item.title,
-                excerpt: item.excerpt,
-                image: item.images[0],
-                category: item.category,
-              }}
-              className={`h-[400px] ${cardClassName}`}
-              imageClassName="h-[250px] object-cover select-none"
-            />
+    <>
+      <p className="text-sm font-bold">
+        {heading}
+      </p>
+      <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={12}
+        slidesPerView={4}
+        navigation
+        className={`flex flex-col gap-3 ${className}`}
+        breakpoints={{
+          // when window width is >= 640px
+          320: {
+            slidesPerView: 1,
+          },
+          // small screens
+          640: {
+            slidesPerView: 1,
+          },
+          // medium screens
+          768: {
+            slidesPerView: 2,
+          },
+          // large screens
+          1024: {
+            slidesPerView: 3,
+          },
+          1280: {
+            slidesPerView: 4,
+          },
+          // extra large screens
+          1536: {
+            slidesPerView: 4,
+          },
+        }}
+      >
+        {!!data && data.length > 0 ? (
+          data.map((item: any) => (
+            <SwiperSlide key={item._id}>
+              <Card
+                item={{
+                  title: item.title,
+                  excerpt: item.excerpt,
+                  image: item.images[0],
+                  category: item.category,
+                }}
+                className={`h-[400px] ${cardClassName}`}
+                imageClassName="h-[250px] object-cover select-none"
+              />
+            </SwiperSlide>
+          ))
+        ) : (
+          <SwiperSlide>
+            <Skeleton />
           </SwiperSlide>
-        ))
-      ) : (
-        <SwiperSlide>
-          <Skeleton />
-        </SwiperSlide>
-      )}
-    </Swiper>
+        )}
+      </Swiper>
+    </>
   );
 };
