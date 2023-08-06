c455ec956b6d24cec24bb83c4d671fa946996337
Gastón Mitoire <gastonmm@gmail.com>
2023-08-06 11:42:24 -0300

    fix: navigation skeleton

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -25,23 +25,27 @@ export const Navigation: React.FC<LinkProps> = ({
       <div className="flex py-3">
         <div className="flex-1"></div>
         <div className="flex-auto flex justify-center">
-          {!!links
-            ? links.map((link) => (
-                <Link
-                  key={link.slug}
-                  href={`/noticias/${link.slug}`}
-                  className={`font-bold text-gray-400 hover:text-gray-800 px-4 ${
-                    activeClass && pathname === `/noticias/${link.slug}`
-                      ? "text-gray-800"
-                      : ""
-                  }`}
-                >
-                  {link.name}
-                </Link>
-              ))
-            : [1, 2, 3, 4, 5].map((n) => (
-                <Skeleton key={n} className="h-5 w-20" />
+          {!!links ? (
+            links.map((link) => (
+              <Link
+                key={link.slug}
+                href={`/noticias/${link.slug}`}
+                className={`font-bold text-gray-400 hover:text-gray-800 px-4 ${
+                  activeClass && pathname === `/noticias/${link.slug}`
+                    ? "text-gray-800"
+                    : ""
+                }`}
+              >
+                {link.name}
+              </Link>
+            ))
+          ) : (
+            <div className="flex-auto flex justify-center gap-5">
+              {[1, 2, 3, 4, 5].map((n) => (
+                <Skeleton key={n} className="h-10 w-32" />
               ))}
+            </div>
+          )}
         </div>
         <div className="flex-1"></div>
       </div>
