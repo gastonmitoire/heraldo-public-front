ab73b0bd9b376adcab4075a6986abdf6e5157fda
Gastón Mitoire <gastonmm@gmail.com>
2023-08-10 02:10:53 -0300

    feat: next revalidate fetch

--- app/utils/fetchClient.ts
+++ app/utils/fetchClient.ts
@@ -1,5 +1,13 @@
-export const fetchClient = (url: string, options: any): Promise<any> => {
-  return fetch(process.env.NEXT_PUBLIC_API_URL + url, options)
+export const fetchClient = (url: string, options: {}): Promise<any> => {
+  const defaultOptions = {
+    next: {
+      revalidate: 3600,
+    },
+  };
+
+  const mergedOptions = { ...defaultOptions, ...options };
+
+  return fetch(process.env.NEXT_PUBLIC_API_URL + url, mergedOptions)
     .then((res) => res.json())
     .catch((err) => {
       throw err;
