4a87545b37c9a9029cc05dc12edc6b26318e2858
Gastón Mitoire <gastonmm@gmail.com>
2023-08-11 06:24:21 -0300

    fix: heading styles

--- app/components/Heading.tsx
+++ app/components/Heading.tsx
@@ -11,9 +11,9 @@ interface HeadingProps {
 
 export const Heading: React.FC<HeadingProps> = ({ title, link, className }) => {
   return (
-    <nav className="flex justify-between gap-5 pb-3 w-full px-3 xl:px-0">
+    <nav className="flex items-center justify-between gap-5 pb-3 w-full px-3 xl:px-0">
       <h2 className="flex-0 text-2xl font-bold">{title}</h2>
-      <div className="flex-auto flex items-center">
+      <div className="flex-auto hidden lg:flex items-center">
         <div className="w-full border-b border-gray-200"></div>
       </div>
       {link && (
