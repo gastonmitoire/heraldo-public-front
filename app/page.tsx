f74c534ae1734be8c5a9608700f7543a0def7c01
Gastón Mitoire <gastonmm@gmail.com>
2023-08-14 13:41:27 -0300

    fix: removed currency failing fetch

--- app/page.tsx
+++ app/page.tsx
@@ -83,7 +83,7 @@ export default async function Home() {
       <section className="container flex flex-col gap-5 mx-auto">
         <AdServerComponent position={AdServerPositions.horizontal3} />
 
-        <CurrencyAndRiver />
+        {/* <CurrencyAndRiver /> */}
 
         <AdServerComponent position={AdServerPositions.horizontal4} />
       </section>
